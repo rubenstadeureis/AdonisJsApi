bf69fbf788f5e9cf0868c9a4315029b8efab5c22
Rubens <rubenstadeureis@hotmail.com>
2023-01-11 10:20:10 -0300

    first

---
+++ app/Controllers/Http/MomentsController.ts
@@ -0,0 +1,94 @@
+import type { HttpContextContract } from '@ioc:Adonis/Core/HttpContext'
+
+import { v4 as uuidv4 } from 'uuid'
+
+import Application from '@ioc:Adonis/Core/Application'
+
+import Moment from 'App/Models/Moment'
+
+export default class MomentsController {
+  private validateOptions = {
+    types: ['image'],
+    size: '2mb',
+  }
+
+  public async store({ request, response }: HttpContextContract) {
+    const body = request.body()
+
+    const image = request.file('image', this.validateOptions)
+
+    if (image) {
+      const imageName = `${uuidv4()}.${image.extname}`
+
+      await image.move(Application.tmpPath('uploads'), {
+        name: imageName,
+      })
+
+      body.image = imageName
+    }
+
+    const moment = await Moment.create(body)
+
+    response.status(201)
+    return {
+      message: 'Momento criado com sucesso',
+      data: moment,
+    }
+  }
+
+  public async index() {
+    const moments = await Moment.all()
+    return {
+      data: moments,
+    }
+  }
+
+  public async show({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
+
+    return {
+      data: moment,
+    }
+  }
+
+  public async destroy({ params }: HttpContextContract) {
+    const moment = await Moment.findOrFail(params.id)
+
+    await moment.delete()
+
+    return {
+      message: 'momento excluido com sucesso ',
+      moment,
+    }
+  }
+
+  public async update({ params, request }: HttpContextContract) {
+    const body = request.body()
+
+    const moment = await Moment.findOrFail(params.id)
+
+    moment.title = body.title
+    moment.description = body.description
+
+    if (moment.image != body.image || !moment.image) {
+      const image = request.file('image', this.validateOptions)
+
+      if (image) {
+        const imageName = `${uuidv4()}.${image!.extname}`
+
+        await image.move(Application.tmpPath('uploads'), {
+          name: imageName,
+        })
+
+        moment.image = imageName
+      }
+    }
+
+    await moment.save()
+
+    return {
+      message: 'Momento Atualizado com sucesso',
+      data: moment,
+    }
+  }
+}
